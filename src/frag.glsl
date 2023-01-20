1727ce6ff609e2788213264b69d6069401953ec8
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2023-01-20 12:47:16 -0600

    demo version

--- src/frag.glsl
+++ src/frag.glsl
@@ -4,5 +4,5 @@ varying vec3 vDepth;
 
 void main() {
 
-    gl_FragColor = vec4(0.7 + (vDepth.z * 2.0), 0.7 + (vDepth.z * 2.0), 0.5 + (vDepth.z * 1.0), 0.8s);
+    gl_FragColor = vec4(0.7 + (vDepth.z * 2.0), 0.7 + (vDepth.z * 2.0), 0.5 + (vDepth.z * 1.0), 0.8);
 }

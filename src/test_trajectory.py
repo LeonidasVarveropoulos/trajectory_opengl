f26ad7f89ff1410c0ed1bc290de939e98b178231
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2023-02-22 18:27:24 -0600

    test

--- src/test_trajectory.py
+++ src/test_trajectory.py
@@ -1,25 +1,56 @@
 from trajectory import Trajectory
 import math
+import numpy as np
 
 def test_trajectory():
     traj = Trajectory()
 
-    points = 50
-    count = 0
+    with open('Camera_point.txt') as f:
+        array = []
+        count = 0
+        for line in f: # read rest of lines
+            array.append([float(x) for x in line.split()])
+            new_array = array[0]
+            new_array[0] = -new_array[0]
+            new_array[1] = -new_array[1]
+            new_array[2] = -new_array[2]
+            print(new_array)
+            print(output_npc(new_array))
+            test = output_npc(new_array)
+            traj.add_point(count, [test[0], test[1], test[2]], [0,0,0])
+            array = []
+            count+=1
+            print("__________")
+        traj.interpolate(200)
+        return traj
+    return None
 
-    while (count <= points):
-        t = count/10.0
-        radius = 1.0/4.0
-        x = radius*(math.sin(t)+2.0*math.sin(2.0*t))
-        y = radius*(math.cos(t)-2.0*math.cos(2.0*t))
-        z = radius*(-math.sin(3.0*t))
+def output_npc(arr):
+    in_array = np.matrix([[arr[0]], [arr[1]], [arr[2]], [1]])
 
-        lin = [x, y, z]
+    fx = 1745.417743800858
+    fy = 1744.804342507085
+    x0 = 1029.543761373819
+    y0 = 653.2514270158324
+    far = 0.5
+    near = 0.001
+    cols = 1920#current_image.shape[1]
+    rows = 1080#current_image.shape[0]
 
-        ang = [50 * math.sin(t), 50 * math.cos(t), 50 * math.sin(t)]
-        traj.add_point(count, lin, ang)
+    perspective = np.matrix([[(-2.0*fx/cols), 0, (cols-2*x0)/cols, 0],
+                            [0, 2.0*fy/rows, -(rows-2*y0)/rows, 0],
+                            [0, 0, 0, 0],
+                            [0, 0, -1, 0]])
+    
+    out = perspective * in_array
+    out/=out[3]
+    return out
+    
+    # traj = Trajectory()
 
-        count+=1
+    # traj.add_point(0, [0.01697442/4, -0.00520116/4, 0.10529092], [0,0,0])
+    # traj.add_point(1, [0.01697442/3, -0.00520116/3, 0.10529092 * 1.1], [0,0,0])
+    # traj.add_point(2, [0.01697442/2, -0.00520116/2, 0.10529092 * 1.3], [0,0,0])
+    # traj.add_point(3, [0.01697442, -0.00520116, 0.10529092], [0,0,0])
 
-    traj.interpolate(200)
-    return traj
+    # traj.interpolate(200)
